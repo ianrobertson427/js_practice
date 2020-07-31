8939e8db9be5a54ae5d9ad6d741daa304578b1e7
“irobetson” <ianrobertson427@gmail.com>
2020-07-30 19:35:13 -0500

    JS Practice

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('hello from js');
+
+
+const months = ["January", "February", "March", "April", "May", "June", "July"];
+
+const random = Math.floor(Math.random() * months.length);
+console.log(random, months[random]);
+
+console.log(moment().format('dddd'));
+
+console.log(moment("19941031", "YYYYMMDD").fromNow());
