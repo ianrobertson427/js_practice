90ee3e9a3e835cb51a5aa225a7543045541cae07
“irobetson” <ianrobertson427@gmail.com>
2020-07-30 20:43:16 -0500

    Adding webpack

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+
+var moment = require('moment');
+// var moment = require('math');
+
+
 console.log('hello from js');
 
 
 const months = ["January", "February", "March", "April", "May", "June", "July"];
+
+
 
 const random = Math.floor(Math.random() * months.length);
 console.log(random, months[random]);
@@ -9,3 +16,7 @@ console.log(random, months[random]);
 console.log(moment().format('dddd'));
 
 console.log(moment("19941031", "YYYYMMDD").fromNow());
+
+// console.log(round(e, 3);
+
+
